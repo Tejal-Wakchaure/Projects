217b7515074d4f1fe7492db4e1ec222fb0a5e563
Tejal Wakchaure <108021526+Tejal-Wakchaure@users.noreply.github.com>
2022-07-10 22:53:22 +0530

    Add files via upload

---
+++ Web-BasedProjects/PlacementManagementSystem/includes/footer.php
@@ -0,0 +1,54 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+         <ul class="bottom">
+                <li><h2>Campus Placement Management System</h2>
+        </li>
+         </ul>
+            
+        <div class="col-md-4 footer-grid">
+            <h4></h4>
+            <ul class="bottom">
+                <li>Designed By: Tejal Wakchaure</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4></h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="tejal@gmail.com">tejal@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>9021091791</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>MKSSS's Cummin College of Engineering For Women, Karvenagar</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: (+91) 9021091791  </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; Tejal Wakchaure, MKSSS's Cummin College of Engineering For Women, Karvenagar</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
