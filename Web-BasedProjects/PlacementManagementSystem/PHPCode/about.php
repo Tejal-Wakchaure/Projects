ff6a5d6644546e49089da6b378cfbcf85a69e414
Tejal Wakchaure <108021526+Tejal-Wakchaure@users.noreply.github.com>
2022-07-10 22:56:14 +0530

    Add files via upload

---
+++ Web-BasedProjects/PlacementManagementSystem/PHPCode/about.php
@@ -0,0 +1,122 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
+                    <div class="clearfix"></div>
+                    <p class="nam" style="color: black">The project is aimed at developing an application
+for the “WEB BASED CAMPUS PLACEMENT MANAGEMENT
+SYSTEM” of the college. The system is an application that can
+be accessed and effectively used throughout the organization
+with proper login enabled. This system can be used as an
+application for the Placement Officers in the college to manage
+the student information with regard to placement. Student
+logging should be able to upload their personal and
+educational information in the form of a resume. The key
+feature of this project is that it is one time registration enabled.
+Our project provides the facility of maintaining the details of
+the students. It reduces the manual work and consumes less
+paper work to reduce the time. This project is developed with
+PHP for frontend and MY SQL for backend. </p>
+
+                </div>
+                <!--div class="top-inner second">
+                    <div class="col-md-6 top-text">
+                        <a href="single.html"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="col-md-6 top-text two">
+                        <a href="single.html"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Team Members: <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f4.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two">2479-Tejal Wakchaure</h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two">2477-Sanskruti Dandekar</h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two">2466-Vaishnavi Marne</h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two">2465-Shruti Vaishnav</h5>
+                            <div class="td-post-date two"> </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                     <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two">2423-Tejal Patil</h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
